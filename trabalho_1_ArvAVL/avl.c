bfe85f1dc92af2a5b25d5042a805486d15e720c0
Annyel Cordeiro <annyel.silva@ufpi.edu.br>
2023-10-28 17:38:10 -0300

    AVL

--- trabalho_1_ArvAVL/avl.c
+++ trabalho_1_ArvAVL/avl.c
@@ -231,4 +231,213 @@ void remover_serie(Serie **raiz, int codigo){
       }
 }
 
+void imprime(Serie *raiz){
+       printf("\nDados da série:\nCodigo: %d\nTitulo: %s\nNumero de temporadas: %d\n\n", raiz->codigo, raiz->titulo, raiz->numTemp);
+}
 
+void imprime_todas_Series(Serie *raiz){
+      if (raiz != NULL)
+      {
+            imprime_Serie(raiz->esq);
+            imprime(raiz);
+            imprime_Serie(raiz->dir);
+      }
+}
+
+void imprime_Serie_Por_Codigo(Serie *raiz, int codigo){
+      if (raiz != NULL)
+      {
+            if (raiz->codigo == codigo)
+            {
+                  imprime(raiz);
+            }else
+            {
+                  if (raiz->esq == codigo)
+                  {
+                        imprime_Serie_Por_Codigo(raiz->esq, codigo);
+                  }else{
+                        imprime_Serie_Por_Codigo(raiz->dir, codigo);
+                  }
+            }
+      }
+}
+
+Temporada criaNoTemp(int cod,int numTemp, char titulo[], int quantidade_Ep, char ano, Part *list){
+      Temporada *new = (Temporada*) malloc(sizeof(Temporada));
+      new->codigo = cod;
+      new->numTem = numTemp;
+      new->participantes = *list;
+      new->ano = ano;
+      strcpy(new->titulo, titulo);
+      new->quantEp = quantidade_Ep;
+      new->esq = new->dir = new->alt = NULL;
+      return *new;
+}
+
+int calcula_Altura_temp(Temporada *no){
+      int i = -1;
+      if (no != -1)
+      {
+            i = no->alt;
+      }
+      return i;
+}
+
+void atualizar_Altura_temp(Temporada *no){
+
+      int altura_esq = calcula_Altura_temp(no->esq);
+      int altura_dir = calcula_Altura_temp(no->dir);
+      no->alt = (altura_esq > altura_dir ? altura_esq : altura_dir) + 1;
+}
+
+int fator_Balanciamento_temp(Temporada *no){
+      int i = 0;
+      int alt_esq, alt_dir;
+      if (no != NULL)
+      {
+            alt_esq = calcula_Altura_temp(no->esq);
+            alt_dir = calcula_Altura_temp(no->dir);
+            i = (alt_esq) - (alt_dir);
+      }
+      return i;
+}
+
+void rotacao_Direita_temp(Temporada **no){
+      Temporada *aux = (*no)->esq;
+      (*no)->esq = aux->dir;
+      aux->dir = (*no);
+      atualizar_Altura_temp(*no);
+      atualizar_Altura_temp(aux);
+
+      (*no) = aux;
+}
+
+void rotacao_Esquerda_temp(Temporada **no){
+      Temporada *aux = (*no)->dir;
+      (*no)->dir = aux->esq;
+      aux->esq = (*no);
+
+      atualizar_Altura_temp(*no);
+      atualizar_Altura_temp(aux);
+
+      (*no) = aux;      
+}
+
+void insere_Temp(Temporada **raiz, Temporada *temp){
+      int fator_de_Balanceamento;
+
+      if (*raiz == NULL)
+      {
+            *raiz = temp;
+      } else if (temp->codigo < (*raiz)->codigo)
+      {
+            insere_Temp(&(*raiz)->esq, temp);
+      }else
+      {
+            insere_Temp(&(*raiz)->dir, temp);
+      }
+
+      fator_de_Balanceamento = fator_Balanciamento_temp(*raiz);
+
+      if (fator_de_Balanceamento == 2)
+      {
+            int fator_balanceamento_esq;
+
+            fator_balanceamento_esq = fator_Balanciamento_temp((*raiz)->esq);
+            if (fator_balanceamento_esq < 0)
+            {
+                  rotacao_Esquerda_temp(&((*raiz)->esq));
+            }
+            rotacao_Direita_temp(*raiz);
+      }else if (fator_de_Balanceamento == -2)
+      {
+             int fator_balanceamento_dir;
+
+             fator_balanceamento_dir = fator_Balanciamento_temp((*raiz)->dir);
+             if (fator_balanceamento_dir > 0)
+             {
+                  rotacao_Direita_temp(&((*raiz)->dir));
+             }
+             rotacao_Esquerda_temp(*raiz);
+      }
+      atualizar_Altura_temp(*raiz);
+}
+
+void busca_folha_temp(Temporada **filho_1, Temporada *filho_2){
+      if (*filho_1)
+      {
+            busca_folha_Serie(&((*filho_1)->dir), filho_2);
+      }else
+      {
+            (*filho_1) = filho_2;
+      }
+}
+
+void remover_temporada(Temporada **raiz, int codigo){
+      if (*raiz == NULL)
+      {
+            Temporada *aux;
+            if ((*raiz)->codigo == codigo)
+            {
+                  if ((*raiz)->esq == NULL && (*raiz)->dir == NULL)
+                  {
+                        free(*raiz);
+                        (*raiz) = NULL;
+                  }else if ((*raiz)->esq == NULL || (*raiz)->dir == NULL)
+                  {
+                        Temporada *id_filho;
+                        if ((*raiz)->esq != NULL)
+                        {
+                              aux = (*raiz);
+                              id_filho = (*raiz)->esq;
+                              (*raiz) = id_filho;
+                              free(aux);
+                              aux = NULL;
+                        }else
+                        {
+                              aux = *raiz;
+                              id_filho = (*raiz)->dir;
+                              *raiz = id_filho;
+                              free(aux);
+                              aux = NULL;                              
+                        }
+                  }else
+                  {
+                        Temporada *filho;
+                        aux = *raiz;
+                        filho = (*raiz)->esq;
+                        busca_folha_temp(&((*raiz)->esq), (*raiz)->dir);
+                        *raiz = filho;
+                        free(aux);      
+                        aux = NULL;                          
+                  }
+                  
+            }
+            else if ((*raiz)->codigo < codigo)
+            {
+                  remover_temporada(&(*raiz)->esq, codigo);
+            }else
+            {
+                  remover_temporada(&(*raiz)->dir, codigo);
+            }
+            
+            if (*raiz != NULL){
+              int fb;
+              fb = fator_Balanciamento_temp(*raiz);
+              if (fb == 2){
+                  if (fator_Balanciamento_temp((*raiz)->esq) == -1){
+                      rotacao_Esquerda_temp(&((*raiz)->esq));
+                  }
+                  rotacao_Direita_temp((raiz));
+
+              } else if (fb == -2){
+                  if (fator_Balanciamento_temp((*raiz)->esq) == 1){
+                      rotacao_Direita_temp(&((*raiz)->dir));
+                  }
+                  rotacao_Esquerda_temp((raiz));
+
+              }
+              atualizar_Altura_temp(*raiz);
+            }      
+      }
+}
--- trabalho_1_ArvAVL/avl.h
+++ trabalho_1_ArvAVL/avl.h
@@ -8,12 +8,13 @@ typedef struct serie{
 //código, título, número de temporadas
 
 typedef struct temporada{
+      int codigo;
       int numTem;
       char titulo[50];
       int quantEp;
       char ano;
       Part participantes;
-      Temporada *esq, *dir;
+      Temporada *esq, *dir, *alt;
 }Temporada;
 //número da temporada, título, quantidade de episódios, ano, endereço para ula lista simples  de participantes.
 
@@ -23,27 +24,32 @@ typedef struct participantes
       Part *prox; 
 }Part;
 
-int calcula_Altura(Serie *no);
-void atualizar_Altura(Serie *no);
-int fator_Balanciamento(Serie *no);
-void rotacao_Direita(Serie **no);
-void rotacao_Esquerda(Serie **no);
 Serie *criaNoSerie(int codigo, char titulo[], int numTemp);
 Serie *busca_Serie_Codigo(Serie **serie, int codigo);
 void *busca_Serie_remove(Serie **serie, int codigo, int codigo_temp);
 void busca_folha_Serie(Serie **ultimo, Serie *no_Filho);
 void remover_serie(Serie **raiz, int codigo);
 Serie *endereco(Serie *raiz);
-
-
 void insere_Serie(Serie **raiz, Serie *serie);
-void imprime_Serie(Serie *raiz);
-int imprime_Serie_Por_Codigo(Serie *raiz, int codigo);
+void imprime_todas_Series(Serie *raiz);
+int calcula_Altura(Serie *no);
+void atualizar_Altura(Serie *no);
+int fator_Balanciamento(Serie *no);
+void rotacao_Direita(Serie **no);
+void rotacao_Esquerda(Serie **no);
+
+void imprime(Serie *raiz);
+
+void imprime_Serie_Por_Codigo(Serie *raiz, int codigo);
 void imprime_Todos_Personagens(Serie *raiz, int codigo);
 void imprime_Personagens_Determinada_Temp(Serie *raiz, int codigo);
-void busca_folha(Serie **filho_1, Serie *filho_2);
 
-Temporada criaNoTemp(int numTemp, char titulo[], int quantidade_Ep, char ano, Part *list);
-
-
+void busca_folha_temp(Temporada **filho_1, Temporada *filho_2);
+Temporada criaNoTemp(int cod,int numTemp, char titulo[], int quantidade_Ep, char ano, Part *list);
+void insere_Temp(Temporada **raiz, Temporada *temp);
 void remover_temporada(Temporada **raiz, int codigo);
+int calcula_Altura_temp(Temporada *no);
+void atualizar_Altura_temp(Temporada *no);
+int fator_Balanciamento_temp(Temporada *no);
+void rotacao_Direita_temp(Temporada **no);
+void rotacao_Esquerda_temp(Temporada **no);
